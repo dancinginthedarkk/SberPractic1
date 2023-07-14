6bc7e920bbfced9f6d094768e163aa6a9a750c70
Dubrovsky18 <dubrovin.02@list.ru>
2023-07-14 16:21:02 +0300

    Move main-page to Frontend service

--- main-page/src/styles/footer.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const FooterContainer = styled.div`
-  height: 70px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 32px;
-`;
